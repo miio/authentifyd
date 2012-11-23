eb8db4221068d51ddafc14d3b8d5546a426fbb43
NicoArbogast <nicolas.arbogast@gmail.com>
2012-11-23 11:37:06 +0100

    override devise controllers via config

--- lib/authentifyd.rb
+++ lib/authentifyd.rb
@@ -2,4 +2,13 @@ require 'devise'
 require "authentifyd/engine"
 
 module Authentifyd
+
+  mattr_accessor :devise_config
+  def self.devise_config
+    @@devise_config = @@devise_config || {}
+    @@devise_config.reverse_merge({
+      :registrations_controller   => 'Authentifyd::Registrations',
+      :sessions_controller        => 'Authentifyd::Sessions'
+    })
+  end
 end
