62af93a35379aca55547df7c4570fd2ddd2b0250
NicoArbogast <nicolas.arbogast@gmail.com>
2012-11-26 19:32:11 +0100

    added path_prefix

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Authentifyd::Engine.routes.draw do
                     sessions: Authentifyd.devise_config[:sessions_controller]}
   }
 
-  match 'auth/:provider/callback', :controller => 'Authentications', :action => "create"
-  match 'auth/failure', :controller => 'Authentications', :action => "failure"
+  match "#{Authentifyd.path}/auth/:provider/callback", :controller => 'Authentications', :action => "create"
+  match "#{Authentifyd.path}/auth/failure", :controller => 'Authentications', :action => "failure"
 
-  match 'authentications/:user_id/link', :controller => 'Authentications', :action => "link", :as => :link_accounts
-  match 'authentications/:user_id/add', :controller => 'Authentications', :action => "add", :as => :add_account
+  match "#{Authentifyd.path}/authentications/:user_id/link", :controller => 'Authentications', :action => "link", :as => :link_accounts
+  match "#{Authentifyd.path}/authentications/:user_id/add", :controller => 'Authentications', :action => "add", :as => :add_account
 end
