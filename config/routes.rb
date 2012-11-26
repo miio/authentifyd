ff19b7af869a2c640603043be76e1323983b2fe4
NicoArbogast <nicolas.arbogast@gmail.com>
2012-11-26 15:49:21 +0100

    fixed routes  + 0.0.0.5

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Authentifyd::Engine.routes.draw do
                     sessions: Authentifyd.devise_config[:sessions_controller]}
   }
 
-  match 'auth/:provider/callback', :controller => 'Authentifyd::Authentications', :action => "create"
-  match 'auth/failure', :controller => 'Authentifyd::Authentications', :action => "failure"
+  match 'auth/:provider/callback', :controller => 'Authentications', :action => "create"
+  match 'auth/failure', :controller => 'Authentications', :action => "failure"
 
-  match 'authentications/:user_id/link', :controller => 'Authentifyd::Authentications', :action => "link", :as => :link_accounts
-  match 'authentications/:user_id/add', :controller => 'Authentifyd::Authentications', :action => "add", :as => :add_account
+  match 'authentications/:user_id/link', :controller => 'Authentications', :action => "link", :as => :link_accounts
+  match 'authentications/:user_id/add', :controller => 'Authentications', :action => "add", :as => :add_account
   root :to => 'home#index'
 end
