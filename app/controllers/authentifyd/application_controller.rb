ba58cf8815bdaeb9ecd6fce2b63da139115cbf85
NicoArbogast <nicolas.arbogast@gmail.com>
2012-11-26 23:00:38 +0100

    callback_url

--- app/controllers/authentifyd/application_controller.rb
+++ app/controllers/authentifyd/application_controller.rb
@@ -5,6 +5,6 @@ end
 
 class ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
-    session[:referrer] ? session[:referrer] : root_path
+    params[:callback_url] || session[:referrer] || root_path
   end
 end
