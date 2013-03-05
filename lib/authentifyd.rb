84f7fcc85eaf28020992b1aa00895a57df4f0507
NicoArbogast <nicolas.arbogast@gmail.com>
2013-03-05 13:06:20 +0100

    added custom css

--- lib/authentifyd.rb
+++ lib/authentifyd.rb
@@ -6,7 +6,7 @@ require "authentifyd/engine"
 
 module Authentifyd
 
-  mattr_accessor :devise_config, :omniauth_config, :path, :path_prefix
+  mattr_accessor :devise_config, :omniauth_config, :path, :path_prefix, :custom_css
 
   def self.devise_config
     (@@devise_config || {}).reverse_merge({
@@ -22,5 +22,4 @@ module Authentifyd
   def self.embeddable_callback_path(_path)
     (Authentifyd.path_prefix ?  "#{Authentifyd.path_prefix}/" : '') + Authentifyd.path + _path
   end
-
 end
