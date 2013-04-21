daa9fd8911a24b4e4744ebceb04fe58d80e46b01
NicoArbogast <nicolas.arbogast@gmail.com>
2013-04-21 23:27:59 +0200

    allow erb yaml files for omniauth configs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,19 +2,19 @@
 facebook_config = Rails.root.to_s + "/config/services/facebook.yml"
 if File.exist?(facebook_config)
   facebook_apps = File.read(facebook_config)
-  FACEBOOK = YAML.load(facebook_apps)[Rails.env].symbolize_keys
+  FACEBOOK = YAML.load(ERB.new(File.read(facebook_apps)).result)[Rails.env].symbolize_keys
 end
 
 twitter_config = Rails.root.to_s + "/config/services/twitter.yml"
 if File.exist?(twitter_config)
   twitter_apps = File.read(twitter_config)
-  TWITTER = YAML.load(twitter_apps)[Rails.env].symbolize_keys
+  TWITTER = YAML.load(ERB.new(File.read(twitter_apps)).result)[Rails.env].symbolize_keys
 end
 
 google_config = Rails.root.to_s + "/config/services/google.yml"
 if File.exist?(google_config)
   google_apps = File.read(google_config)
-  GOOGLE = YAML.load(google_apps)[Rails.env].symbolize_keys
+  GOOGLE = YAML.load(ERB.new(File.read(google_apps)).result)[Rails.env].symbolize_keys
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
